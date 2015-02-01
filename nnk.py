e958ea6bc1b6b45d8d0c4ade3dfd65ae512d8982
666thdevil <666thdevil@gmail.com>
2015-02-01 11:39:40 +0530

    Maths

---
+++ nnk.py
@@ -0,0 +1,16 @@
+import math
+t = int(input())
+list = []
+i = 0
+while(t!=0):
+	a,b = int(input()), int(input())
+	if (b%4 == 0):
+		x=math.pow(a,4)
+		list.append(x%10)
+	else:
+		x=math.pow(a,(b%4))
+		list.append(x%10)
+	t=t-1
+
+for x in list:
+	print(int(x))
